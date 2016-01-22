b3ccfe6921b583172981c14b7b016d56976bddb0
정기남 <jkn5202@naver.com>
2016-01-22 14:19:32 +0900

    no message

--- src/com/java/controller/Controller.java
+++ src/com/java/controller/Controller.java
@@ -60,7 +60,7 @@ public class Controller {
 		System.out.println();
 		System.out.println("");
 		System.out.println();
-		System.out.println("");
+		System.out.println("brach");
 		
 		while(true){
 			
